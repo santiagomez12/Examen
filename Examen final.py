bd41b4a3648e59b26df5b74af9a42e32c7199939
santiagomez12 <96155613+santiagomez12@users.noreply.github.com>
2022-04-01 17:40:59 -0500

    Add files via upload

---
+++ Examen final.py
@@ -0,0 +1,35 @@
+import math 
+
+g=9.8 
+def funcion1(angulo,rozamiento,masa1):
+  masa2=0 
+  angulo=math.radians(angulo) 
+  seno=math.sin(angulo)
+  coseno=math.cos(angulo)
+  result=[]
+  while True:
+    aceleracion=g*(masa1*seno-masa2-rozamiento*masa1*coseno)/(masa1+masa2)
+    if aceleracion<0:
+      result.append([masa2,"cae"])
+      break
+    else:
+      result.append([masa2,"sube"]) 
+      masa2+=0.5
+  return result
+      
+print(funcion1(30,0.2,16))
+
+def funcion2(masa1,masa2,rozamiento):
+  for angulo in range(10,86):
+    angulo=math.radians(angulo)
+    seno=math.sin(angulo)
+    coseno=math.cos(angulo)
+    aceleracion=g*(masa1*seno-masa2-rozamiento*masa1*coseno)/(masa1+masa2)
+    
+    aceleracion=round(aceleracion,1)
+    
+    if aceleracion==0:
+      return math.degrees(angulo)
+  return -1
+
+print(funcion2(7,4,0.2))
